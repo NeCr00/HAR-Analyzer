6707e6257b0beb7dd4d9b5132e6c29234c96e1d3
cgiannos <37047743+cgiannos@users.noreply.github.com>
2021-07-29 18:02:37 +0300

    Add files via upload

---
+++ results.php
@@ -0,0 +1,28 @@
+<?php include('navbar.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <title>Upload Har File</title>
+  <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css" />
+  <style>
+        #map { width: 1000px; height: 1000px; }
+    </style>
+    <script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/heatmapjs@2.0.2/heatmap.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/leaflet-heatmap@1.0.0/leaflet-heatmap.js"></script>
+    <script src="sales.json"></script>
+</head>
+
+<body>
+
+  <h1> Optimized Ip Map </h1>
+
+
+  <div id="map"></div>
+    <script src="results.js"></script>
+  
+</body>
