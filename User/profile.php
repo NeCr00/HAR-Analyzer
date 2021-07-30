fb1c3027ba186b82f57fe8be118976eb001e6fa4
Magaiver007 <58440856+magaiver007@users.noreply.github.com>
2021-07-30 23:49:52 +0300

    Add files via upload

---
+++ User/profile.php
@@ -0,0 +1,50 @@
+
+<?php 
+#include('navbar.php'); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <title>Upload Har File</title>
+    <link rel="stylesheet" href="profile.css">
+    <script defer src="fileUpload.js"></script>
+</head>
+
+<body>
+
+ <div class="card-container">
+	<img class="round" src="\images\69.jpg" alt="user" />
+	<h3> Η μάνα του Γιάννου</h3>
+	<h6>Τοποθεσία: Στα Δικαστήρια</h6>
+	<div class="buttons">
+		<button class="primary">
+			Change Username
+		</button>
+		<button class="primary">
+			Change Password
+		</button>
+	</div>
+	<div class="skills">
+		<h6>Στατιστικά δεδομένα</h6>
+		<ul>
+			<li>Τελευταίο upload: 4/20/69</li>
+			<li>Πλήθος εγγραφών: 666</li>
+			
+		</ul>
+	</div>
+</div>
+
+<footer>
+	<p>
+		Created by
+		<a target="_blank" href="">Ntiri</a>
+		- Read how I became your dad
+		<a target="_blank" href="">here</a>
+	</p>
+</footer>
+
+</html>
