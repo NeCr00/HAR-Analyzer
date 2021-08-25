dcb429bc0d48a8b8e1305cd4670a2e3a36d36df1
Ioannis Christodoulakos <36794189+NeCr00@users.noreply.github.com>
2021-08-25 04:02:16 +0300

    Adding icon to navbar

--- User/navbar.php
+++ User/navbar.php
@@ -19,22 +19,23 @@ if (isset($_SESSION['username']) && $_SESSION['role'] == 'Admin') {
     <title>Forum</title>
     <link rel="stylesheet" href="navbar.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </header>
 
 <body class="news">
     <header>
         <div class="nav">
-        <div class =logo>
-                
-                </div>
+            <div class=logo>
+
+            </div>
             <ul>
-            <li style="float:left;"><img class=logo src="../images/logo.png"></li>
-                <li ><a id="link"  href="fileUpload.php">Har Upload</a></li>
-                <li ><a id="link" href="profile.php">Profile Management</a></li>
-                
+                <li style="float:left;"><img class=logo src="../images/logo.png"></li>
+                <li><a id="link" href="fileUpload.php">Har Upload</a></li>
+                <li><a id="link" href="profile.php">Profile Management</a></li>
+
                 <li><a id="link" href="results.php">Data Visualization</a></li>
-                <li class="logout"><a id="link" href="/logout.php">Log Out</a></li>
+                <li class=logout><a id="link" href="/logout.php">Log Out <img  src="../images/logout.png"/></a></li>
+                
             </ul>
 
 
